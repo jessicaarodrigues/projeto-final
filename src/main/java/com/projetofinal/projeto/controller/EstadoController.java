403f9ecfff267e9404efd3f95d54c0b7f90d78f3
Jessica Alexandra Rodrigues dos Santos <j.alexandra.r61@gmail.com>
2021-03-23 16:38:42 -0300

    Atualizando

--- src/main/java/com/projetofinal/projeto/controller/EstadoController.java
+++ src/main/java/com/projetofinal/projeto/controller/EstadoController.java
@@ -2,13 +2,14 @@ package com.projetofinal.projeto.controller;
 
 import com.projetofinal.projeto.converter.EstadoConverter;
 import com.projetofinal.projeto.dto.EstadoDTO;
+import com.projetofinal.projeto.model.Estado;
 import com.projetofinal.projeto.service.EstadoService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/estados")
 public class EstadoController {
-    private final EstadoService estadoService;
+    private EstadoService estadoService = new EstadoService();
     private final EstadoConverter estadoConverter;
 
     public EstadoController(EstadoConverter estadoConverter) {
@@ -17,8 +18,8 @@ public class EstadoController {
     }
 
     @PostMapping("")
-    public EstadoDTO create(RequestBody CreateEstadoDTO estado) {
-        Estado mappedEstado = this.estadoConverter.convertToEntity(estados);
+    public <estado> EstadoDTO create(RequestBody CreateEstadoDTO estado) {
+        Estado mappedEstado = this.estadoConverter.convertToEntity(estado);
         return this.estadoConverter.convertToDTO(this.estadoService.create(mappedEstado));
     }
 
--- src/main/java/com/projetofinal/projeto/dto/CreateEstadoDTO.java
+++ src/main/java/com/projetofinal/projeto/dto/CreateEstadoDTO.java
@@ -4,19 +4,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.jetbrains.annotations.NotNull;
 
 @NoArgsConstructor
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PUBLIC)
-public class EstadoDTO {
-    private Long id;
-
-    @NotNull
+public class CreateEstadoDTO {
     private String nome;
-
-    @NotNull
     private String uf;
-
-
 }
